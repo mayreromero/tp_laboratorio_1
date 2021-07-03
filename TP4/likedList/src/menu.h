dc9118fbd20f0d8181d574081c5cb19c9a3056bd
Mayre Romero <52429741+mayreromero@users.noreply.github.com>
2021-07-03 16:15:42 -0300

    Delete TP4/likedList directory

--- TP4/likedList/src/menu.h
+++
@@ -1,7 +0,0 @@
-
-#ifndef SRC_MENU_H_
-#define SRC_MENU_H_
-
-int menu();
-
-#endif /* SRC_MENU_H_ */
--- TP4/likedList/src/parser.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef SRC_PARSER_H_
-#define SRC_PARSER_H_
-
-int parser_EmployeeFromText(FILE* pFile , LinkedList* pArrayListEmployee);
-int parser_EmployeeFromBinary(FILE* pFile , LinkedList* pArrayListEmployee);
-
-#endif /* SRC_PARSER_H_ */
